a9a93508196c79f6a28ed1f2a92605fa8feaf60f
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-12-30 19:28:29 +0200

    initial commit

---
+++ src/main/java/com/example/userloginform/UserLoginFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.userloginform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserLoginFormApplication.class, args);
+    }
+
+}
