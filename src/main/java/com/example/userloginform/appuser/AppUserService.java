0013ca7a1c9348538bbd89620b7d7458fd2f5c16
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2024-01-02 09:47:03 +0200

    initial commit
    not working
    // TODO: javax.mail doesn't exist and should be replaced with something another

--- src/main/java/com/example/userloginform/appuser/AppUserService.java
+++ src/main/java/com/example/userloginform/appuser/AppUserService.java
@@ -1,5 +1,7 @@
 package com.example.userloginform.appuser;
 
+import com.example.userloginform.registration.token.ConfirmationToken;
+import com.example.userloginform.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,33 +9,66 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Service
 @AllArgsConstructor
 public class AppUserService implements UserDetailsService {
+
     private final static String USER_NOT_FOUND_MSG =
-            "User with email %s not found";
+            "user with email %s not found";
+
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email)
                 .orElseThrow(() ->
-                        new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+                        new UsernameNotFoundException(
+                                String.format(USER_NOT_FOUND_MSG, email)));
     }
-    public String singUpUser(AppUser appUser) {
-        boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
 
-        if(userExists) {
+    public String signUpUser(AppUser appUser) {
+        boolean userExists = appUserRepository
+                .findByEmail(appUser.getEmail())
+                .isPresent();
+
+        if (userExists) {
+            // TODO check of attributes are the same and
+            // TODO if email not confirmed send confirmation email.
+
             throw new IllegalStateException("email already taken");
         }
-       String encodedPassword = bCryptPasswordEncoder
-               .encode(appUser.getPassword());
+
+        String encodedPassword = bCryptPasswordEncoder
+                .encode(appUser.getPassword());
+
         appUser.setPassword(encodedPassword);
 
         appUserRepository.save(appUser);
 
-        //TODO: SEND CONFIRMATION TOKEN
-        return "it works";
+        String token = UUID.randomUUID().toString();
+
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(
+                confirmationToken);
+
+//        TODO: SEND EMAIL
+
+        return token;
+    }
+
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
     }
 }
